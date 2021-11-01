2bf0e4697477618544fa584ea48f96f5dbc6ca34
Yishay Hazan <yishay11013@gmail.com>
2021-11-01 23:38:05 +0200

    v1.0

---
+++ README.md
@@ -0,0 +1,162 @@
+# import
+
+Install the files the [current version](https://github.com/Yishay666/y2/v1.0) into `your_directory/y2`,
+then import the files
+
+```html
+<head>
+  <link rel="stylesheet" href="./y2/y2.css" />
+</head>
+<script src="./y2/y2.js"></script>
+```
+
+# First, SplitText
+
+to start with any animation - you have to split your paragraph into lines, words and letters, <br/>
+to do that use the `splitText()` function
+
+the functions first argument will be the selector of the paragraph<br/>
+(unique, im using `document.querySelector()`).
+
+the second argument is not required,<br/>it's a callback function that will execute when the `splitText()` function is finished.
+
+it will look something like this:
+
+```javascript
+y2.splitText("p", cb);
+```
+
+<br/>
+Now, You can make your own animations with your paragraph - or use my awesome built-in animations.
+
+#### important notes
+
+inside the paragraph, you cannot use any tags, signs or emojies.
+
+**The formatting syntax:**<br/>
+use `[my awesome paragraph..]` to make the text inside bold.<br/>
+use `{my awesome paragraph..}` to make the text italic.</br>
+use `_awesome paragraph.._` for underline.</br>
+
+for every line, you should use `^`, **and no spaces around**.<br/>
+like this: `my first line^my second line`.
+
+and of course, you can do something like `{[_my paragraph is too cool_]}`
+
+and if you want to use one of this signs - `[{_^` just put `~` before, works for `~` also.
+
+# typing()
+
+![typing-animation-gif](https://user-images.githubusercontent.com/50710472/139737136-937846ed-7e24-49a9-be43-4a1121d048bf.gif)
+
+basic syntax:
+
+```javascript
+y2.animate("p", { type: "typing" });
+```
+
+functions first argument is the selector, then settings, and a callback function (not required).
+in settings, you can define a lot of variables,
+
+```javascript
+type: 'typing', // the type of the animation
+time: 7, // how long the it will take the javascript to add the right classes to the html
+duration: 0.5, // duration for the css animation
+color: 'red', // any valid css color
+transform: 'translateY(5px) rotate(-10deg)', // any valid transform value
+textShadow: "2px 2px orange" // any valid text-shadow value
+```
+
+**the code for the gif above:**
+
+```javascript
+y2.animate(
+  "p",
+  {
+    type: "typing",
+    time: 7,
+    duration: 0.5,
+    color: "red",
+    transform: "translateY(5px) rotate(-10deg)",
+    textShadow: "2px 2px orange",
+  },
+  () => alert("I will run when the animation is over!")
+);
+```
+
+> Note: everything is not required except `type:`
+
+# sendColors()
+
+![sendColors-animation-gif (1)](https://user-images.githubusercontent.com/50710472/139739220-2e1b16a7-d91e-4aa8-88d2-c6545a8d7ffb.gif)
+
+basic syntax:
+
+```javascript
+y2.animate("p", { type: "sendColors" });
+```
+
+functions first argument is the selector, then settings, and a callback function (not required).
+in settings, you can define a lot of variables,
+
+```javascript
+type: 'sendColors', // the type of the animation
+time: 7, // how long the it will take the javascript to add the right classes to the html
+duration: 0.5, // duration for the css animation
+color: 'red', // any valid css color
+// instead of settings one color, you set 3 for gradient effect
+color_1: 'red', color_2: 'blue', color_3: 'green', // any valid color accepted
+jump: -15, // the letters can take a little jump, set to negative value for it to go up
+textShadow: "2px 2px orange" // any valid text-shadow value
+```
+
+the gif above on action:
+
+```javascript
+y2.animate(
+  "p",
+  {
+    type: "sendColors",
+    time: 7,
+    duration: 0.5,
+    color: "red",
+    jump: -5,
+    color: "red",
+    textShadow: "2px 2px orange",
+  },
+  () => console.log("I will run when the animation is over")
+);
+```
+
+you can also do this:
+
+```javascript
+y2.animate(
+  "p",
+  {
+    type: "sendColors",
+    time: 7,
+    duration: 0.5,
+    color_1: "red",
+    color_2: "blue",
+    color_3: "yellow",
+    jump: -5,
+    color: "red",
+    textShadow: "2px 2px orange",
+  },
+  () => console.log("I will run when the animation is over")
+);
+```
+
+> Note: everything is not required except `type:`
+
+# End
+
+I created this library because every other option in pretty shitty, or costs you money (hm hhm gsap),
+you can use my code however you want, its not that good anyways,
+
+if you have anything to say / ask / approve / complain / demand,
+Im on discord [Post Malone#0376](https://discord.com/channels/me/484834908758605824), or on twitter [@yishayhaz)](https://twitter.com/yishayhaz),
+
+for now, lets party <br/>
+![pepe-pepe-drinking](https://user-images.githubusercontent.com/50710472/139740244-6fd77ed6-1d8d-431d-a8da-8d0b22df2877.gif)
---
+++ y2.js
@@ -0,0 +1,147 @@
+const y2 = {
+    splitText: (who, cb) => {
+        splitText(who, cb)
+    },
+    animate: (who, settings, cb) => {
+        switch(settings.type) {
+            case 'typing':
+                return typing(who, settings, cb);
+            case 'sendColors':
+                return sendColors(who, settings, cb);
+        }
+    }
+}
+
+function splitText(who, cb='undefined') {
+    var obj = document.querySelector(who);
+    obj.classList.add('splitted_text');
+    txt = obj.innerHTML.trim();
+
+    var underline = false;
+    var skip = false;
+    var newHTML = "<section class='line'><div class='word'>";
+    
+    newHTML += txt.split('').map(l => {
+        if(!skip){
+            switch(l) {
+                case '~':
+                    skip = true;
+                    break;
+                case ' ':
+                    return '</div> <div class="word">'
+                case '|':
+                    return '<br/>';
+                case '[':
+                    return '<b>';
+                case ']':
+                    return '</b>';
+                case '{':
+                    return '<i>';
+                case '}':
+                    return '</i>';
+                case '_':
+                    underline = !underline;
+                    return;
+                case '^':
+                    return '</div></section><section class="line"><div class="word">';
+                default:
+                    return underline ? "<span class='letter'><u>"+l+"</u></span>" : "<span class='letter'>"+l+"</span>";
+            }
+        } else { 
+            skip = false;
+            if(underline) {
+                return "<span class='letter'><u>"+l+"</u></span>"
+            } else {
+                return "<span class='letter'>"+l+"</span>"
+            }
+        }
+        
+    }).join('');
+    newHTML += '</div></section>'
+    obj.innerHTML = newHTML;
+    if(typeof cb === 'function') cb();
+}
+
+let root = document.documentElement.style;
+let data = {}
+
+function typing(who, settings, cb='undefined') {
+    // function variables
+    var spans = document.querySelectorAll(`${who} .letter`);
+    time = settings.time || 5;
+
+    // set up data
+    data.i = 0;
+    data.spans = spans;
+    data.speed = (time * 1000) / spans.length;
+    data.className = 'y2_type',
+    data.before_className = 'y2_before_type',
+
+    // set up data in root css
+    settings.duration = settings.duration || 0.5
+    root.setProperty('--duration', `${settings.duration}s`);
+    root.setProperty('--transform', settings.transform || '');
+    root.setProperty('--opacity', settings.opacity || '1');
+    root.setProperty('--color', settings.color || 'inherit');
+    root.setProperty('--textShadow', settings.textShadow || 'inherit');
+
+    // add before_tag & remove all the tags in case of re-animate
+    spans.forEach(span => span.classList.add(data.before_className));
+    spans.forEach(span => span.classList.remove(data.className));
+
+
+    spans[spans.length-1].addEventListener('animationend', () => {
+        spans.forEach(span => span.classList.remove(data.className));
+        if(cb != 'undefined') cb();
+    })
+    // animate them inside
+    sendAnimation();
+}
+
+function sendColors(who, settings, cb='undefined') {
+    // function variables
+    var spans = document.querySelectorAll(`${who} .letter`);
+    time = settings.time || 5;
+
+    // set up data
+    data.i = 0;
+    data.spans = spans;
+    data.speed = (time * 1000) / spans.length;
+    data.className = 'y2_sendColors',
+    data.before_className = null,
+
+    // set up data in root css
+    settings.duration = settings.duration || 0.5;
+    settings.jump = settings.jump || 0;
+    settings.textShadow = settings.textShadow || 'inherit';
+
+    root.setProperty('--duration', `${settings.duration}s`);
+    root.setProperty('--color_1', settings.color_1 || settings.color || 'green');
+    root.setProperty('--color_2', settings.color_2 || settings.color || 'green');
+    root.setProperty('--color_3', settings.color_3 || settings.color || 'green');
+    root.setProperty('--transform', `translateY(${settings.jump}px)`);
+    root.setProperty('--textShadow', settings.textShadow);
+
+    // add before_tag & remove all the tags in case of re-animate
+    spans.forEach(span => span.classList.add(data.before_className));
+    spans.forEach(span => span.classList.remove(data.className));
+
+
+    spans[spans.length-1].addEventListener('animationend', () => {
+        spans.forEach(span => span.classList.remove(data.className));
+        if(cb != 'undefined') cb();
+    })
+    // animate them inside
+    sendAnimation();
+}
+// THE function
+function sendAnimation(){
+    if (data.i < data.spans.length) {
+        data.spans[data.i].classList.add(data.className);
+        data.spans[data.i].classList.remove(data.before_className);
+        data.i++;
+        setTimeout(() => {
+            sendAnimation();
+        }, data.speed);
+    }
+}
